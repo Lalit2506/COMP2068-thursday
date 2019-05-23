1cfa4830aff0f4c962e845c54cddb8e95eddf41b
Lalit Sharma <lalit255290@gmail.com>
2019-05-23 16:51:32 -0400

    Dynamic port assignment

--- app.js
+++ app.js
@@ -10,4 +10,5 @@ app.get('/about', (req, res) => {
  res.send(`I like to play games`);
 });
 
-app.listen(4000, () => console.log('Listening on 4000'));
+const port = (process.env.PORT || 4000);
+app.listen(port, () => console.log(`listening on ${port}`));
